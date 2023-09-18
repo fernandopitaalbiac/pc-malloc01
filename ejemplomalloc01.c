5d6b87d1cbeceed4129422c41dee0f2a06ef1e16
fernandopitaalbiac <72222256+fernandopitaalbiac@users.noreply.github.com>
2023-09-18 14:45:12 +0200

    Update ejemplomalloc01.c
    Ejemplo simple de un programa con malloc

--- ejemplomalloc01.c
+++ ejemplomalloc01.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    
+    // Usamos malloc para asignar memoria para un arreglo de enteros de tamaño n
+    int *arreglo = (int *)malloc(n * sizeof(int));
+    
+    // Verificamos si la asignación de memoria fue exitosa
+    if (arreglo == NULL) {
+        printf("Error al asignar memoria.\n");
+        return 1; // Terminamos el programa con un código de error
+    }
+    
+    // Llenamos el arreglo con algunos valores
+    for (int i = 0; i < n; i++) {
+        arreglo[i] = i * 2;
+    }
+    
+    // Imprimimos los valores del arreglo
+    printf("Valores del arreglo:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arreglo[i]);
+    }
+    printf("\n");
+    
+    // Liberamos la memoria asignada con malloc
+    free(arreglo);
+    
+    return 0;
+}
 
